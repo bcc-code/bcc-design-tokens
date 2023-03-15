51306aaaffadd65f5f62d342f0178fa9ce919b2d
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-15 16:34:57 +0100

    Transform basic colors from Figma tokens to Tailwind config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export { default as bccForbundetTheme } from "./tailwind/bccForbundetTheme";
+export { fontFamily } from "./tailwind/fontFamily";
+export { colors } from "./tailwind/colors";
+export { backgroundColor } from "./tailwind/backgroundColor";
+export { borderColor } from "./tailwind/borderColor";
+export { textColor } from "./tailwind/textColor";
---
+++ src/tailwind/textColor.ts
@@ -0,0 +1,11 @@
+export const textColor = {
+  "primary": "#111827",
+  "secondary": "#4b5563",
+  "tertiary": "#9ca3af",
+  "warning": "#724611",
+  "danger": "#8b102f",
+  "success": "#0a5d2f",
+  "info": "#194e8f",
+  "system": "#0d5154",
+  "brand": "#004e48"
+};
